67085227db34af5b8aebfe9b990d5b8bc3e8ecbd
bbek3201 <bbek3201@gmail.com>
2026-03-30 10:53:22 +0800

    This project is complete

--- src/components/TaskCounter.jsx
+++ src/components/TaskCounter.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const TaskCounter = ({ tasks, deleteCompleteTasks }) => {
   return (
     <div className="flex justify-between gap-19 border-t-2 border-gray-300 w-[340px] pt-[16px]">
-      <p className="pl-5 text-gray-500">
+      <p className="pl-5 text-gray-400">
         {tasks.filter((t) => t.isCompleted).length} of {tasks.length}
         is complete
       </p>
